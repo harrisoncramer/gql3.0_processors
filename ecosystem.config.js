5e410f16ccb46d64334f64316a95ff926e7494d3
Harrison Cramer <harrisoncramer@gmail.com>
2020-06-08 19:24:12 -0400

    trying again...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -28,7 +28,8 @@ module.exports = {
       repo: process.env.GIT_REPO,
       path: process.env.PROJECT_PATH,
       /// Install Linux dependencies
-      "post-setup": "./installLinuxDeps.sh",
+      "pre-setup":
+        "sudo apt install gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libgbm1",
       "pre-deploy-local": `./deployEnvs.sh ${process.env.PROJECT_PATH} ${hostsBashArgs}`,
       "post-deploy": `source ~/.zshrc && \
        yarn install --ignore-engines && \
