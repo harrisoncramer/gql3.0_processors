8a35da5cd8fc5a89fcca251b22df5f2a2f4f361c
Harrison Cramer <harrisoncramer@gmail.com>
2020-06-15 23:17:47 -0400

    added scraper for hfac

--- processor/scrapers/index.js
+++ processor/scrapers/index.js
@@ -2,6 +2,7 @@ import puppeteerv1 from "./types/puppeteerv1";
 import puppeteerv2 from "./types/puppeteerv2";
 import puppeteerv4 from "./types/puppeteerv4";
 import puppeteerv5 from "./types/puppeteerv5";
+import unlimited from "./types/unlimited";
 
 export const pickScraper = (type) =>
   ((type) => {
@@ -14,6 +15,8 @@ export const pickScraper = (type) =>
         return puppeteerv4;
       case "puppeteerv5":
         return puppeteerv5;
+      case "unlimited":
+        return unlimited;
       default:
         throw new Error("That routine doesn't exist!");
     }
---
+++ processor/scrapers/types/unlimited.js
@@ -0,0 +1,82 @@
+import randomUser from "random-useragent";
+
+import { getPageData, getLinks, openNewPages } from "../internals";
+import { logger } from "../../loggers/winston";
+import { setPageBlockers, setPageScripts } from "../../setup/config";
+import { asyncForEach } from "../../../util";
+
+export default async (browser, job) => {
+  // Setup initial list of links
+  let baseLink = job.phaseOne.baseLink;
+  let allLinks = job.phaseOne.range.map((x) =>
+    baseLink.replace("SUBSTITUTE", x)
+  );
+
+  let results = [];
+
+  console.log(job.phaseOne.range);
+
+  await asyncForEach(allLinks, async (link) => {
+    // Create new page
+    let page;
+    try {
+      page = await browser.newPage();
+      let userAgentString = randomUser.getRandom();
+      await page.setUserAgent(userAgentString);
+    } catch (err) {
+      logger.error("Could not navigate to inital page. ", err);
+      throw err;
+    }
+
+    let links;
+    // Go to link and get all sub-links
+    try {
+      await page.goto(link);
+      await setPageBlockers(page);
+      await setPageScripts(page);
+      links = await getLinks({
+        page,
+        selectors: job.phaseTwo,
+      });
+    } catch (err) {
+      logger.error(`Could not get links for link ${link}: `, err);
+    }
+
+    // Create a new page for each link
+    let pages;
+    try {
+      pages = await openNewPages(browser, links);
+    } catch (err) {
+      logger.error("Could not navigate to pages. ", err);
+      throw err;
+    }
+
+    // For each page, get the data
+    let pageData;
+    try {
+      pageData = await getPageData({
+        pages,
+        selectors: job.phaseThree,
+      });
+    } catch (err) {
+      logger.error("Could not get pageData. ".err);
+      throw err;
+    }
+
+    // Close each page
+    try {
+      let pages = await browser.pages();
+      await Promise.all(pages.map(async (page) => await page.close()));
+    } catch (err) {
+      logger.error("Could not close pages. ", err);
+      throw err;
+    }
+
+    console.log(`Finished with page ${link}`);
+
+    // Add the pageData to the results array
+    results.push(...pageData);
+  });
+
+  return results;
+};
